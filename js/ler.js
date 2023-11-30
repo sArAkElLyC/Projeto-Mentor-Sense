bfeacd20f5584c175296583a02d2940268d86d59
Sara <sarakellycardosofischer53@gmail.com>
2023-11-30 16:17:12 -0300

    Subindo o site

---
+++ js/ler.js
@@ -0,0 +1,188 @@
+// Select button1
+var button1 = document.getElementById('btn1');
+
+// Click Event
+button1.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card1');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button1.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button1.textContent = 'Ler mais';
+});
+
+
+// Select button2
+var button2 = document.getElementById('btn2');
+
+// Click Event
+button2.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card2');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button2.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button2.textContent = 'Ler mais';
+});
+
+// Select button3
+var button3 = document.getElementById('btn3');
+
+// Click Event
+button3.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card3');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button3.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button3.textContent = 'Ler mais';
+});
+
+
+
+// Select button4
+var button4 = document.getElementById('btn4');
+
+// Click Event
+button4.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card4');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button4.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button4.textContent = 'Ler mais';
+});
+
+
+// Select button5
+var button5 = document.getElementById('btn5');
+
+// Click Event
+button5.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card5');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button5.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button5.textContent = 'Ler mais';
+});
+
+
+
+// Select button6
+var button6 = document.getElementById('btn6');
+
+// Click Event
+button6.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card6');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button6.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button6.textContent = 'Ler mais';
+});
+
+
+// Select button7
+var button7 = document.getElementById('btn7');
+
+// Click Event
+button7.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card7');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button7.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button7.textContent = 'Ler mais';
+});
+
+
+// Select button8
+var button8 = document.getElementById('btn8');
+
+// Click Event
+button8.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card8');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button8.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button8.textContent = 'Ler mais';
+});
+
+
+// Select button9
+var button9 = document.getElementById('btn9');
+
+// Click Event
+button9.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('#card9');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button9.textContent = 'Ler menos';
+    }
+    
+    // Change button text if hasn't class active
+    button9.textContent = 'Ler mais';
+});
